95da597f916cf892c243fdabf0b3774a5dafa45d
MichalAW <michal.wanta@gmail.com>
2019-03-08 16:00:09 +0100

    hamburger fix

--- js/javascript.js
+++ js/javascript.js
@@ -5,4 +5,21 @@ function toggleTopbar(){
 toggle.addEventListener("click", function(){
   toggleTopbar();
 });
- 
+
+// obsluga topbar
+var links = document.querySelectorAll('.topbar a');
+var sections = document.querySelectorAll('section');
+
+// nadanie eventu na wszystkich linkach z topbara
+for (var i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', function() {
+
+    // usuwanie klasy active ze wszystkich section
+    for (var i = 0; i < sections.length; i++) {
+      sections[i].classList.remove('active');
+    }
+
+    // nadawanie klasy active kliknietej z topbar sekcji
+    document.querySelector(this.hash).classList.add('active');
+  });
+}
