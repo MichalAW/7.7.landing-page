f8d43d61b9b40b9a380e6c8831da5749e76c2a91
MichalAW <michal.wanta@gmail.com>
2019-03-17 15:01:12 +0100

    fixes for header, footer, rwd, and about us

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,21 @@
-function toggleTopbar(){
-  document.getElementById('topbar').classList.toggle('active');
-}
-
-toggle.addEventListener("click", function(){
-  toggleTopbar();
-});
-
-// obsluga topbar
 var links = document.querySelectorAll('.topbar a');
 var sections = document.querySelectorAll('section');
 
-// nadanie eventu na wszystkich linkach z topbara
-for (var i = 0; i < links.length; i++) {
-  links[i].addEventListener('click', function() {
+function toggleTopbar(){
+  	document.getElementById('topbar').classList.toggle('active');
+}
 
-    // usuwanie klasy active ze wszystkich section
+// event for all links in topbar
+for (var i = 0; i < links.length; i++) {
+  	links[i].addEventListener('click', function() {
+
+    // remove class active from all sections
     for (var i = 0; i < sections.length; i++) {
       sections[i].classList.remove('active');
     }
+});
 
-  });
+toggle.addEventListener("click", function(){
+  	toggleTopbar();
+});
 }
